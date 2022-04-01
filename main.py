bdb3f9cc3bbb1a15c0480b0f33d3fd1f13106f8f
Khaled Eldesoukey <54212167+ImmortalBoi@users.noreply.github.com>
2022-04-01 05:28:43 +0200

    Added some notes

---
+++ main.py
@@ -0,0 +1,14 @@
+KEYWORDS = [
+    "and"       ,"break"     ,"do"        ,"else"      ,"elseif"
+    "end"       ,"false"     ,"for"       ,"function"  ,"if"
+    "in"        ,"local"     ,"nil"       ,"not"       ,"or"
+    "repeat"    ,"return"    ,"then"      ,"true"      ,"until"     ,"while"
+]
+OPERATORS = [
+    "+"     ,"-"     ,"*"     ,"/"     ,"%"     ,"^"     ,"#"
+    "=="    ,"~="    ,"<="    ,">="    ,"<"     ,">"     ,"="
+    "("     ,")"     ,"{"     ,"}"     ,"["     ,"]"
+    ";"     ,":"     ,","     ,"."     ,".."    ,"..."  
+]
+with(open("./IOfiles/input.lua",'r')) as f:
+    f.readlines()
